75f91ea4f43e1ce345af58f49dd46e99da9001ba
Sharik Mahmood <smahmood@creativeelectron.com>
2025-01-09 16:05:18 -0800

    got GUI to write to file and take input from JSON, just need to fix the file path stuff

--- generated/front_window.py
+++ generated/front_window.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'TimeshseetUIRMkqsA.ui'
+## Form generated from reading UI file 'TimeshseetUIYncRSn.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.1
 ##
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QDateEdit, QDialog,
     QDialogButtonBox, QGridLayout, QLabel, QLineEdit,
-    QSizePolicy, QWidget)
+    QPushButton, QSizePolicy, QWidget)
 
 class Ui_DialogTimeSheet(object):
     def setupUi(self, DialogTimeSheet):
@@ -28,37 +28,10 @@ class Ui_DialogTimeSheet(object):
         DialogTimeSheet.setMaximumSize(QSize(415, 155))
         self.gridLayout = QGridLayout(DialogTimeSheet)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label_sup_name_title = QLabel(DialogTimeSheet)
-        self.label_sup_name_title.setObjectName(u"label_sup_name_title")
-
-        self.gridLayout.addWidget(self.label_sup_name_title, 1, 0, 1, 1)
-
-        self.pushButton_finish = QDialogButtonBox(DialogTimeSheet)
-        self.pushButton_finish.setObjectName(u"pushButton_finish")
-        self.pushButton_finish.setOrientation(Qt.Orientation.Horizontal)
-        self.pushButton_finish.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
-
-        self.gridLayout.addWidget(self.pushButton_finish, 4, 1, 2, 2)
-
-        self.lineEdit_emp_name_input = QLineEdit(DialogTimeSheet)
-        self.lineEdit_emp_name_input.setObjectName(u"lineEdit_emp_name_input")
-
-        self.gridLayout.addWidget(self.lineEdit_emp_name_input, 0, 1, 1, 1)
-
         self.label_pay_period_start_title = QLabel(DialogTimeSheet)
         self.label_pay_period_start_title.setObjectName(u"label_pay_period_start_title")
 
         self.gridLayout.addWidget(self.label_pay_period_start_title, 2, 0, 1, 1)
-
-        self.lineEdit_file_path_input = QLineEdit(DialogTimeSheet)
-        self.lineEdit_file_path_input.setObjectName(u"lineEdit_file_path_input")
-
-        self.gridLayout.addWidget(self.lineEdit_file_path_input, 3, 1, 1, 1)
-
-        self.lineEdit_sup_name_input = QLineEdit(DialogTimeSheet)
-        self.lineEdit_sup_name_input.setObjectName(u"lineEdit_sup_name_input")
-
-        self.gridLayout.addWidget(self.lineEdit_sup_name_input, 1, 1, 1, 1)
 
         self.label_file_path_title = QLabel(DialogTimeSheet)
         self.label_file_path_title.setObjectName(u"label_file_path_title")
@@ -70,10 +43,42 @@ class Ui_DialogTimeSheet(object):
 
         self.gridLayout.addWidget(self.label_emp_name_title, 0, 0, 1, 1)
 
+        self.label_sup_name_title = QLabel(DialogTimeSheet)
+        self.label_sup_name_title.setObjectName(u"label_sup_name_title")
+
+        self.gridLayout.addWidget(self.label_sup_name_title, 1, 0, 1, 1)
+
+        self.lineEdit_sup_name_input = QLineEdit(DialogTimeSheet)
+        self.lineEdit_sup_name_input.setObjectName(u"lineEdit_sup_name_input")
+
+        self.gridLayout.addWidget(self.lineEdit_sup_name_input, 1, 1, 1, 1)
+
         self.dateEdit_date_input = QDateEdit(DialogTimeSheet)
         self.dateEdit_date_input.setObjectName(u"dateEdit_date_input")
 
         self.gridLayout.addWidget(self.dateEdit_date_input, 2, 1, 1, 1)
+
+        self.lineEdit_emp_name_input = QLineEdit(DialogTimeSheet)
+        self.lineEdit_emp_name_input.setObjectName(u"lineEdit_emp_name_input")
+
+        self.gridLayout.addWidget(self.lineEdit_emp_name_input, 0, 1, 1, 1)
+
+        self.pushButton_finish = QDialogButtonBox(DialogTimeSheet)
+        self.pushButton_finish.setObjectName(u"pushButton_finish")
+        self.pushButton_finish.setOrientation(Qt.Orientation.Horizontal)
+        self.pushButton_finish.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
+
+        self.gridLayout.addWidget(self.pushButton_finish, 4, 1, 2, 2)
+
+        self.pushButton_path_picker = QPushButton(DialogTimeSheet)
+        self.pushButton_path_picker.setObjectName(u"pushButton_path_picker")
+
+        self.gridLayout.addWidget(self.pushButton_path_picker, 3, 1, 1, 1)
+
+        self.label_path_title = QLabel(DialogTimeSheet)
+        self.label_path_title.setObjectName(u"label_path_title")
+
+        self.gridLayout.addWidget(self.label_path_title, 5, 0, 1, 1)
 
 
         self.retranslateUi(DialogTimeSheet)
@@ -85,9 +90,11 @@ class Ui_DialogTimeSheet(object):
 
     def retranslateUi(self, DialogTimeSheet):
         DialogTimeSheet.setWindowTitle(QCoreApplication.translate("DialogTimeSheet", u"Dialog", None))
-        self.label_sup_name_title.setText(QCoreApplication.translate("DialogTimeSheet", u"Enter Supervisor Name", None))
         self.label_pay_period_start_title.setText(QCoreApplication.translate("DialogTimeSheet", u"Select Pay Period Start", None))
         self.label_file_path_title.setText(QCoreApplication.translate("DialogTimeSheet", u"File Path", None))
         self.label_emp_name_title.setText(QCoreApplication.translate("DialogTimeSheet", u"Emter Employe Name", None))
+        self.label_sup_name_title.setText(QCoreApplication.translate("DialogTimeSheet", u"Enter Supervisor Name", None))
+        self.pushButton_path_picker.setText(QCoreApplication.translate("DialogTimeSheet", u"PushButton", None))
+        self.label_path_title.setText(QCoreApplication.translate("DialogTimeSheet", u"TextLabel", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import generated.front_window as front_window
 import sys
+import shutil
+import os
+import json
+import tkinter
+from tkinter import filedialog
 from PySide6.QtWidgets import QDialog, QApplication
 from PySide6.QtCore import QDate
 from datetime import datetime
@@ -9,72 +14,154 @@ from openpyxl import load_workbook
 # Class to incorperate functions for  
 class My_App:
 
+
+    # Global vars for needed cells
+    EMPLOYE_NAME = "G11"
+    SUPERVI_NAME = "G12"
+    PAY_STA_NAME = "G16"
+    PAY_END_NAME = "G17"
+    PATH = " "
+
+
+
     def __init__(self):
+
+
+        # Get default values
+        # Def read_default_json()
+        # This function checks to see if defualt.json exist. 
+        # if it exist load json file into a dictionary
+        # load defaults dict.get("Key", defualt="")
         self.generate_data()
+
+
 
     # Loads in data for GUI, if pre existing data exists
     def generate_data(self):
-        
-        wb = load_workbook('Excel Test File.xlsx',keep_vba=True, data_only=True)
-        sheet = wb.active   
-        cell_value = sheet['G17'].value
-        
-        that = cell_value
-        print("Cell val here:",that)
-        
-        wb.save('Excel Test File.xlsx')
-        date_str = "1/6/2025"
-        date = QDate.fromString(date_str, "M/d/yyyy") # Convert the string to a QDate object
 
+        # get to needed directory and saves it
+        os.chdir("Timesheet Template")  
+        curr_path = os.getcwd() 
+
+
+        # gets file we will read from
+        file_path_default = os.path.join(curr_path, "default.json") 
+
+
+        # reads in .json to an object
+        if not os.path.isfile(file_path_default):
+            raise FileNotFoundError(f"Error: The file '{file_path_default}' does not exist.") # Check to see if file exist and if it doesnt error out. 
+            exit()
+        with open(file_path_default, 'r') as file:
+            data = json.load(file)
+        
+        
+        #loads Workbook
+        wb = load_workbook('Excel Test File.xlsx',keep_vba=True, data_only=True)  
+
+
+        # Convert the string to a QDate object
+        date = QDate.fromString(data["Pay Period Start"], "M/d/yyyy") 
         self.current_date = datetime.now().strftime("%m/%d/%Y")
 
-        ui.lineEdit_emp_name_input.setText("Sharik Mahmood")
-        ui.lineEdit_sup_name_input.setText("Omar Gonzales")
-        ui.lineEdit_file_path_input.setText("Testing :)")
+
+        # this block of code sets the texts for the GUI
         ui.dateEdit_date_input.setDate(date)
+        ui.lineEdit_emp_name_input.setText(data["Employee Name"])
+        ui.lineEdit_sup_name_input.setText(data["Supervisor Name"])
+        # TODO: We need the previous file path to autofill and be auto selected
+        # If there was not a previous we do the current working directory
 
-
-        # Pay period can be calculated using the calender
-        # File path will be given from the user, can re-use previous location
+        # Saving and closing workbook :)
+        try:
+            wb.save('Excel Test File.xlsx')
+        except:
+            print("This workbook is open, please close it")
+            exit()
     
+
 
     # writes desired data to excel file
     def write_to_file(self):
-        wb = load_workbook('Excel Test File.xlsx')
+
+        # gets path for the file we are going to write to
+        myFile = 'Excel Test File.xlsx'
+       
+       # opens workbook to write to
+        try:
+            wb = load_workbook(myFile)
+        except:
+            print("this workbook is already open, please close it")
+            wb.save(myFile)
+            exit()
+
+        
+        # activates workbook
         sheet = wb.active     
 
-        self.current_pay_period = ui.dateEdit_date_input.text()
-        self.emp_name = ui.lineEdit_emp_name_input.text()
-        self.sup_name = ui.lineEdit_sup_name_input.text()   
 
-        sheet['G11'] = self.emp_name    # Employee name
-        sheet['G12'] = self.sup_name    # Supervisor name
-        sheet['G16'] = self.current_pay_period # Pay Period start date
-        sheet['I41'] = self.emp_name    # Employee signature
+        # creates JSON object and Saves all inputed GUI Items into the a JSON file
+        json_obj = {
+            "Employee Name": ui.lineEdit_emp_name_input.text(),
+            "Supervisor Name": ui.lineEdit_sup_name_input.text(),
+            "Pay Period Start":  ui.dateEdit_date_input.text(), 
+            "File Path": self.PATH
+        }
+        with open('default.json', 'w') as outfile:
+            json.dump(json_obj, outfile, sort_keys=True, indent=4)
+
+
+        # Writes inputed values on to the workbook
+        sheet[self.EMPLOYE_NAME] = ui.lineEdit_emp_name_input.text()    # Employee name
+        sheet[self.SUPERVI_NAME] = ui.lineEdit_sup_name_input.text()   # Supervisor name
+        sheet[self.PAY_STA_NAME] =  ui.dateEdit_date_input.text() # Pay Period start date
+        sheet['I41'] = ui.lineEdit_emp_name_input.text()    # Employee signature
         sheet['E41'] = self.current_date # Current date
+        sheet['A101'] = self.PATH # FilePath that we used
         
-        wb.save('Excel Test File.xlsx')
 
-    # Outputs user input, from the GUI window
-    def Output_Gui(self):
-        print("Employee name:", self.emp_name) 
-        print("Supervisor name:", self.sup_name) 
-        print("Pay Period Date:", ui.dateEdit_date_input.text()) 
-        print("File Path:", ui.lineEdit_file_path_input.text())
+        # saves and closes the workbook :)
+        try:
+            wb.save(myFile)
+        except:
+            print("this workbook is already open, please close it")
+            wb.save(myFile)
+            exit()
 
 
+        # Copys the completed workbook, creates a copy of it, than moves it to the desired location
+        shutil.copy(myFile, self.PATH)
+        cur_path = self.PATH + "\\" + myFile
+        copy = self.PATH + "moving" + ".xlsx"
+        new_path = self.PATH + "\\" + ui.lineEdit_emp_name_input.text() + " Timesheet  " +datetime.now().strftime("%Y-%m-%d %H.%M.%S")  + ".xlsx"
+        os.rename(cur_path, copy)
+        shutil.move(copy, new_path)
+        
+
+
+    # Opens file explorer and saves the file path we choose
+    def set_output_path(self):
+
+        tkinter.Tk().withdraw() 
+        folder_path = filedialog.askdirectory()
+        self.PATH = folder_path
+
+
+
+        
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)  # Create the application instance
     Dialog = QDialog()  # Create the dialog instance
     ui = front_window.Ui_DialogTimeSheet()  # Instantiate the UI class
-
     ui.setupUi(Dialog)  # Set up the UI for the dialog
 
     my_app = My_App() # Creates instance of this class
-
     Dialog.show()  # Show the dialog
-    ui.pushButton_finish.clicked.connect(lambda: my_app.write_to_file()) #if clicked on 'OK' we write to file
+
+    # actions if we click a button
+    ui.pushButton_finish.clicked.connect(lambda: my_app.write_to_file())
+    ui.pushButton_path_picker.clicked.connect(lambda: my_app.set_output_path())
 
     sys.exit(app.exec())  # Run the application
 
